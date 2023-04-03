56a4db63c752cd15a4a5df2e8563e6eb4f807306
diefesson <diefesson.so@gmail.com>
2023-04-03 18:58:56 -0300

    feat: Caesar brute force

---
+++ src/Classic/CaesarBruteForce.cs
@@ -0,0 +1,12 @@
+namespace Diefesson.Cryptool.Classic;
+
+public static class CaesarBruteForce
+{
+    public static string[] bruteForce(string encrypted)
+    {
+        return Enumerable.Range(0, Consts.AlphabetLen).Select(key => {
+            var decrypter = new CaesarDecrypter(key);
+            return String.Concat(encrypted.Select(decrypter.Decrypt));
+        }).ToArray();
+    }
+}
--- src/Main.cs
+++ src/Main.cs
@@ -4,11 +4,11 @@ var key = 3;
 var plain = "My Super Secret Message";
 
 var encrypter = new CaesarEncrypter(key);
-var decrypter = new CaesarDecrypter(key);
 
-var crypted = String.Concat(plain.Select(encrypter.Crypt));
-var decrypted = String.Concat(crypted.Select(decrypter.Decrypt));
+var encrypted = String.Concat(plain.Select(encrypter.Crypt));
 
-Console.WriteLine($"plain: {plain}");
-Console.WriteLine($"crypted: {crypted}");
-Console.WriteLine($"decrypted: {decrypted}");
+var candidates = CaesarBruteForce.bruteForce(encrypted);
+for(var i = 0; i < encrypted.Length; i++)
+{
+    Console.WriteLine($"{i} : {candidates[i]}");
+}
