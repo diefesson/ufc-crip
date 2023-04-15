d2fbaceb65b55e42341c85c696929a4e9cc5800e
diefesson <diefesson.so@gmail.com>
2023-04-15 13:51:43 -0300

    test: fixed SanitizerTest to new punctuation feature

--- Cryptool.Tests/src/SanitizerTests.cs
+++ Cryptool.Tests/src/SanitizerTests.cs
@@ -8,10 +8,11 @@ public class SanitizerTests
     public void Test()
     {
         var text = "O mundo é estranho.\n";
-        Assert.Equal("O MUNDO E ESTRANHO\n", Sanitizer.Sanitize(text, new SanitizerOptions { }));
-        Assert.Equal("O MUNDO  ESTRANHO\n", Sanitizer.Sanitize(text, new SanitizerOptions { convert = false }));
-        Assert.Equal("OMUNDOEESTRANHO\n", Sanitizer.Sanitize(text, new SanitizerOptions { spaces = false }));
-        Assert.Equal("O MUNDO E ESTRANHO", Sanitizer.Sanitize(text, new SanitizerOptions { lines = false }));
-        Assert.Equal("O mundo e estranho\n", Sanitizer.Sanitize(text, new SanitizerOptions { upper = false }));
+        Assert.Equal("O MUNDO E ESTRANHO.\n", Sanitizer.Sanitize(text, new SanitizerOptions { }));
+        Assert.Equal("O MUNDO  ESTRANHO.\n", Sanitizer.Sanitize(text, new SanitizerOptions { convert = false }));
+        Assert.Equal("OMUNDOEESTRANHO.\n", Sanitizer.Sanitize(text, new SanitizerOptions { spaces = false }));
+        Assert.Equal("O MUNDO E ESTRANHO.", Sanitizer.Sanitize(text, new SanitizerOptions { lines = false }));
+        Assert.Equal("O MUNDO E ESTRANHO\n", Sanitizer.Sanitize(text, new SanitizerOptions {punctuation = false}));
+        Assert.Equal("O mundo e estranho.\n", Sanitizer.Sanitize(text, new SanitizerOptions { upper = false }));
     }
 }
